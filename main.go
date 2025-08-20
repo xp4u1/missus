c3052c7ca498b8d0eb6bf0d4cff69db9b1c63b8d
Paul Hoerenz <development@hoerenz.com>
2025-08-20 22:33:31 +0200

    feat: switch to htmx

--- main.go
+++ main.go
@@ -10,34 +10,44 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-//go:embed html/app.html
+//go:embed web/app.html
 var appHTML string
+
+//go:embed web/vendor/tailwind.min.js
+var tailwindJS string
+
+//go:embed web/vendor/htmx.min.js
+var htmxJS string
+
+func p(s string) string {
+	return "<p>" + s + "</p>"
+}
 
 func uploadHandler(c echo.Context) error {
 	file, err := c.FormFile("file")
 	if err != nil {
-		return c.String(http.StatusBadRequest, "missing file")
+		return c.String(http.StatusBadRequest, p("File is missing"))
 	}
 
 	src, err := file.Open()
 	if err != nil {
-		return c.String(http.StatusInternalServerError, "unable to read file")
+		return c.String(http.StatusInternalServerError, p("Unable to read file"))
 	}
 	defer src.Close()
 
 	dst, err := os.Create(file.Filename)
 	if err != nil {
-		return c.String(http.StatusInternalServerError, "unable to save file")
+		return c.String(http.StatusInternalServerError, p("Unable to create file on the server"))
 	}
 	defer dst.Close()
 
 	if _, err = io.Copy(dst, src); err != nil {
-		return c.String(http.StatusInternalServerError, "unable to save file")
+		return c.String(http.StatusInternalServerError, p("Unable to write content"))
 	}
 
 	fmt.Printf("received file: %s\n", file.Filename)
 
-	return c.String(http.StatusOK, "successfully uploaded file")
+	return c.String(http.StatusOK, p("Successfully uploaded "+file.Filename))
 }
 
 func main() {
@@ -47,6 +57,12 @@ func main() {
 		return c.HTML(http.StatusOK, appHTML)
 	})
 	e.POST("/upload", uploadHandler)
+	e.GET("/tailwind.min.js", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, tailwindJS)
+	})
+	e.GET("/htmx.min.js", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, htmxJS)
+	})
 
 	fmt.Println("Starting web interface on :9125") // todo: port flag
 
