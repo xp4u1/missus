f27ea50e249e9ae691b6690b55358845e5195dc0
Paul Hoerenz <development@hoerenz.com>
2025-08-21 20:53:41 +0200

    fix: prevent crash when unable to get local IP address

--- main.go
+++ main.go
@@ -3,42 +3,22 @@ package main
 import (
 	_ "embed"
 	"flag"
-	"io"
 	"log"
 	"net"
 	"strconv"
 )
 
 // GetOutboundIP Ping Cloudflare to get local IP address
-func GetOutboundIP() string {
+func GetOutboundIP() (string, error) {
 	conn, err := net.Dial("udp", "1.1.1.1:80")
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	defer conn.Close()
 
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 
-	return localAddr.IP.String()
-}
-
-func StartServer(addr string) {
-	e := echo.New()
-
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, appHTML)
-	})
-	e.POST("/upload", uploadHandler)
-	e.GET("/tailwind.min.js", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, tailwindJS)
-	})
-	e.GET("/htmx.min.js", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, htmxJS)
-	})
-
-	e.HideBanner = true
-	e.HidePort = true
-	e.Logger.Fatal(e.Start(addr))
+	return localAddr.IP.String(), nil
 }
 
 func main() {
@@ -46,7 +26,13 @@ func main() {
 	qr := flag.Bool("qr", true, "Generate a QR code to connect devices")
 	flag.Parse()
 
-	ip := GetOutboundIP()
+	ip, err := GetOutboundIP()
+	if err != nil {
+		log.Println("unable to determine local address")
+		ip = "localhost"
+		*qr = false
+	}
+
 	addr := ":" + strconv.Itoa(*port)
 
 	log.Println("starting web interface on " + ip + addr)
